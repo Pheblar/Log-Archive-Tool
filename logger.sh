872c393234285a21184e96e59de9b55f48eb05c4
Pheblar <masterrat511@gmail.com>
2025-01-12 19:16:47 +0200

    feat: done

--- logger.sh
+++ logger.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [$# -ne 1]; then
+if [ $# -ne 1 ]; then
     echo "Incorrect number of arguments"
     exit 1
 fi
 
 LOG_DIR="$1"
 
-if [!-d "$LOG_DIR"]; then
+if [ ! -d "$LOG_DIR" ]; then
     echo "Directory doesn't exist"
     exit 1
 fi
@@ -18,10 +18,14 @@ mkdir -p "$OUTPUT_DIR"
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 ARCHIVE_NAME="logs_archive_$TIMESTAMP.tar.gz"
 
-tar -czf"$OUTPUT_DIR/$ARCHIVE_NAME" -C"$LOG_DIR"
-
-echo "Logs archivated from '$LOG_DIR' to $TIMESTAMP" >> archive_log.txt
-echo "Logs compressed at $OUTPUT_DIR/$ARCHIVE_NAME and writen to the log file" 
+if [ "$(ls -A "$LOG_DIR")" ]; then  
+    tar -czf "$OUTPUT_DIR/$ARCHIVE_NAME" -C "$LOG_DIR" .
+    echo "Logs archived from '$LOG_DIR' to $TIMESTAMP" >> archive_log.txt  
+    echo "Logs compressed at $OUTPUT_DIR/$ARCHIVE_NAME and written to the log file."
+else  
+    echo "No files to compress in '$LOG_DIR'"
+    exit 1  
+fi
 
 
 
